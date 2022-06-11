4a252a64524052c74f5517dc074031d8190c2bc3
Antonio Rubén Preixens Prejano <ERTYNEA@hotmail.com>
2022-06-11 23:04:03 +0200

    Modifico script: Convierto el marco a clase

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,54 @@ var windowInnerWidth = window.innerWidth;
 var windowInnerHeight = window.innerHeight;
 var canvas;
 
+// Class framework
+class Framework {
+	constructor(x, y, w, h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+	
+	getX() {
+		return this.x;
+	}
+
+	setX(x) {
+		this.x = x;
+	}
+
+	getY() {
+		return this.y;
+	}
+
+	setY(y) {
+		this.y = y;
+	}
+
+	getW() {
+		return this.w;
+	}
+
+	setW(w) {
+		this.w = w;
+	}
+
+	getH() {
+		return this.h;
+	}
+
+	setH(h) {
+		this.h = h;
+	}
+}
+
 // Var framework
-var frameworkPositionX = 10;
-var frameworkPositionY = 10;
-var frameworkMeasureW = windowInnerWidth - 20;
-var frameworkMeasureH = windowInnerHeight - 20;
+var framework = new Framework(10, 10, windowInnerWidth - 20, windowInnerHeight - 20);
 
 // Var square
-var squarePositionX = frameworkPositionX;
-var squarePositionY = getRandomInt(frameworkPositionY, frameworkMeasureH);
+var squarePositionX = framework.getX();
+var squarePositionY = getRandomInt(framework.getY(), framework.getH());
 var squareMeasureW = 10;
 var squareMeasureH = 10;
 
@@ -20,8 +59,8 @@ function setup() {
 	windowInnerWidth = window.innerWidth;
 	windowInnerHeight = window.innerHeight;
 	canvas = createCanvas(windowInnerWidth, windowInnerHeight);
-	frameworkMeasureW = windowInnerWidth - 20;
-	frameworkMeasureH = windowInnerHeight - 20;
+	framework.setW(windowInnerWidth - 20);
+	framework.setH(windowInnerHeight - 20);
 }
 
 // Window onresize (autocall)
@@ -34,16 +73,16 @@ function draw() {
 	background(255, 0, 0);
 	noStroke();
 	fill(0, 0, 0);
-	rect(frameworkPositionX, frameworkPositionY, frameworkMeasureW, frameworkMeasureH);
+	rect(framework.getX(), framework.getY(), framework.getW(), framework.getH());
 	noStroke();
 	fill(0, 0, 255);
 	rect(squarePositionX, squarePositionY, squareMeasureW, squareMeasureH);
-	if (squarePositionX<frameworkMeasureW) {
+	if (squarePositionX<framework.getW()) {
 		squarePositionX += 10;
 	}
 	else {
-		squarePositionX = frameworkPositionX;
-		squarePositionY = getRandomInt(frameworkPositionY, frameworkMeasureH);
+		squarePositionX = framework.getX();
+		squarePositionY = getRandomInt(framework.getY(), framework.getH());
 	}
 }
 
