a1887d44673808425d2977b232c5a3f9155ee14d
Antonio Rubén Preixens Prejano <ERTYNEA@hotmail.com>
2022-06-12 00:17:03 +0200

    Incluyo classFramework con la clase aparte

--- src/js/script.js
+++ src/js/script.js
@@ -3,48 +3,6 @@ var windowInnerWidth = window.innerWidth;
 var windowInnerHeight = window.innerHeight;
 var canvas;
 
-// Class framework
-class Framework {
-	constructor(x, y, w, h) {
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-	}
-	
-	getX() {
-		return this.x;
-	}
-
-	setX(x) {
-		this.x = x;
-	}
-
-	getY() {
-		return this.y;
-	}
-
-	setY(y) {
-		this.y = y;
-	}
-
-	getW() {
-		return this.w;
-	}
-
-	setW(w) {
-		this.w = w;
-	}
-
-	getH() {
-		return this.h;
-	}
-
-	setH(h) {
-		this.h = h;
-	}
-}
-
 // Var framework
 var framework = new Framework(10, 10, windowInnerWidth - 20, windowInnerHeight - 20);
 
