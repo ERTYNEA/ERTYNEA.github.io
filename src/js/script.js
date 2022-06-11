d34a04101461783747cf01f4ab2535dc0071cbeb
Antonio Ruben Preixens Prejano <ruben.preixens@viseo.com>
2022-06-11 12:41:25 +0200

    Modifico script: se mejora el redimensionamiento

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // Variables ventana
 var ventanaInteriorAncho = window.innerWidth;
 var ventanaInteriorAlto = window.innerHeight;
+var canvas;
 
 // Variables cuadrado
 var posicionX = 0;
@@ -8,18 +9,22 @@ var posicionY = getRandomNumero(0, ventanaInteriorAlto);
 var medidaAnc = 10;
 var medidaAlt = 10;
 
+// Preparacion del canvas (se autollama y se puede llamar manualmente)
 function setup() {
+	ventanaInteriorAncho = window.innerWidth;
+	ventanaInteriorAlto = window.innerHeight;
 	canvas = createCanvas(ventanaInteriorAncho, ventanaInteriorAlto);
 }
 
+// Redimensionamiento del canvas (se autollama)
 window.onresize = function() {
-	ventanaInteriorAncho = window.innerWidth;
-	ventanaInteriorAlto = window.innerHeight;
-	canvas.size(ventanaInteriorAncho, ventanaInteriorAlto);
+	setup();
 }
 
 function draw() {
 	background(255, 0, 0);
+	noStroke();
+	fill(0, 0, 255);
 	rect(posicionX, posicionY, medidaAnc, medidaAlt);
 	if (posicionX<ventanaInteriorAncho) {
 		posicionX += 10;
