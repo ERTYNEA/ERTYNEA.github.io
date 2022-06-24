cb4f97323e993b47c9d2faeaef872fde57db16d3
Antonio Rubén Preixens Prejano <ERTYNEA@hotmail.com>
2022-06-25 00:46:51 +0200

    Incluyo el uso del raton

---
+++ src/js/classMouse.js
@@ -0,0 +1,23 @@
+// Class Mouse
+class Mouse {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    setX(x) {
+        this.x = x;
+    }
+
+    getY() {
+        return this.y;
+    }
+
+    setY(y) {
+        this.y = y;
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 // Var
 var space = new Space(window.innerWidth, window.innerHeight);
 var framework = new Framework(10, 10, space.getW() - 20, space.getH() - 20);
-var character_a = new Character("/img/Human.png", framework.getX(), getRandomInt(framework.getY(), framework.getH()), 10, 10);
+var mouse = new Mouse(framework.getX(), framework.getY());
+var character_a = new Character("/img/Human.png", framework.getX(), framework.getY(), 10, 10);
 var character_b = new Character(null, framework.getX(), getRandomInt(framework.getY(), framework.getH()), 10, 10);
 
 function preload()
@@ -33,17 +34,27 @@ function draw() {
     fill(0, 0, 0);
     rect(framework.getX(), framework.getY(), framework.getW(), framework.getH());
 
+    // character_a
     noStroke();
-    fill(0, 255, 0);
     image(img, character_a.getX(), character_a.getY(), character_a.getW(), character_a.getH());
-    if (character_a.getX() < framework.getW()) {
+    if ((character_a.getX() < framework.getW()) &&
+        (character_a.getX() < mouse.getX())) {
         character_a.setX(character_a.getX() + 1);
     }
-    else {
-        character_a.setX(framework.getX());
-        character_a.setY(getRandomInt(framework.getY(), framework.getH()));
+    else if ((character_a.getX() > framework.getX()) &&
+        (character_a.getX() > mouse.getX())) {
+        character_a.setX(character_a.getX() - 1);
+    }
+    if ((character_a.getY() < framework.getH()) &&
+        (character_a.getY() < mouse.getY())) {
+        character_a.setY(character_a.getY() + 1);
+    }
+    else if ((character_a.getY() > framework.getY()) &&
+        (character_a.getY() > mouse.getY())) {
+        character_a.setY(character_a.getY() - 1);
     }
 
+    // character_b
     noStroke();
     fill(0, 0, 255);
     rect(character_b.getX(), character_b.getY(), character_b.getW(), character_b.getH());
@@ -59,4 +70,9 @@ function draw() {
 // Random int with min (included) and max (excluded)
 function getRandomInt(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function mousePressed() {
+    mouse.setX(mouseX);
+    mouse.setY(mouseY);
 }
