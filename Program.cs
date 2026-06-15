2d420afc4e40f3584062688e9197158f64964590
ERTYNEA <ERTYNEA@hotmail.com>
2026-06-15 11:51:32 +0200

    feat: create default Blazor GitHub Pages app

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-// Blazor WebAssembly application configuration and entry point
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using NewAndRetroPong;
+using ERTYNEA.github.io;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-// Configure application root component
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Configure HTTP client for the application
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// Build and run the application
 await builder.Build().RunAsync();
